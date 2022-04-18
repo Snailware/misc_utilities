072a7e83c2321f8c50074aa5343b4be6738054fd
Snailware <Adamlancaster.al@gmail.com>
2022-04-18 12:47:53 -0400

    optimized names.

--- nic_manager/nic_manager.bat
+++ nic_manager/nic_manager.bat
@@ -1,12 +1,12 @@
 @echo off
 ping -n 181 127.0.0.1>nul
-:main_loop
-    ping -n 1 XXX.XXX.XXX.XXX | findstr TTL || goto reset_nic
+:main
+    ping -n 1 XXX.XXX.XXX.XXX | findstr TTL || goto reset
     ping -n 16 127.0.0.1>nul
-    goto main_loop
-:reset_nic
+    goto main
+:reset
     netsh interface set interface XXXXX disable
     ping -n 16 127.0.0.1>nul
     netsh interface set interface XXXXX enable
     ping -n 181 127.0.0.1>nul
-    goto main_loop
+    goto main
