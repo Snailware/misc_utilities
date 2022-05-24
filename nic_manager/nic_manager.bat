72bd1ee0bd5ed193b36eced0c7510165fc6fcadb
Snailware <Adamlancaster.al@gmail.com>
2022-05-23 20:52:20 -0400

    adjusted timings for better operation.

--- nic_manager/nic_manager.bat
+++ nic_manager/nic_manager.bat
@@ -2,11 +2,11 @@
 ping -n 181 127.0.0.1>nul
 :main
     ping -n 1 XXX.XXX.XXX.XXX | findstr TTL || goto reset
-    ping -n 16 127.0.0.1>nul
+    ping -n 30 127.0.0.1>nul
     goto main
 :reset
     netsh interface set interface XXXXX disable
-    ping -n 16 127.0.0.1>nul
+    ping -n 30 127.0.0.1>nul
     netsh interface set interface XXXXX enable
     ping -n 181 127.0.0.1>nul
     goto main
